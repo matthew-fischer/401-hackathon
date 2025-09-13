712a608a5ba0c515f0ef7b6d6ff4873b887a6ea6
Ellis McDougald <ecmcdougald@gmail.com>
2025-09-13 12:18:01 -0600

    Register models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application, Resume, Response
+
+admin.site.register(Application)
+admin.site.register(Resume)
+admin.site.register(Response)
